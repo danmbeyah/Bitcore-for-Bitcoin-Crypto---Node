57673ea669998b44f3110de88632cc1658d9ea75
Daniel <danmbeyah@gmail.com>
2018-03-15 20:19:00 +0300

    Successful transaction

--- app/routes/bitcoin_routes.js
+++ app/routes/bitcoin_routes.js
@@ -11,24 +11,32 @@ const error_msg = 'You are an imposter, only Lanisters have access to Kings Land
  * m/purpose/coin_type/account/change/address_index
  * coint_type 0=Bitcoin 1=Bitcoin Testnet, account=a/c or user id
  */
-const bip_derive_path = "m/44/1/0/0/0";
+const path = "m/44/1/0/0/0";
+const pathChange = "m/44/1/0/1/0";
 
 var Address = bitcore.address;
+var Networks = bitcore.network;
 
-//Generated using /generate_hd_private_keys API. Store securely in config and not as implemnted here.
+var UnspentOutput = bitcore.Transaction.UnspentOutput;
+
+//Generated using /generate_hd_private_keys API. Generate these on the fly (using path) and not as implemented here.
 const hdPrivateKeys = [
 	"xprv9s21ZrQH143K3PyFnjECi5h582c2Jy1C2tArHAQrxRhfEhkofL4btQCH24vQ9QaLoiwdnNkdNDjntBa1EiCaJaGnx1Qng6byc58HZW9dH1r",
 	"xprv9s21ZrQH143K3VNPdhHXBt9UMbtB3mykBXVmEsfyF1a6M9S3DMqWPzj7zDMtNEQu5N6yGkB86oFQ3fWsRThQ8S5jnXcP3sMJP1qU9QNc9Du",
 	"xprv9s21ZrQH143K2Vmrq4KEQaNXiZ6UfCzJvuCABQcTv2VZfTz2ekkrt6At9ZUScqETr7dZu3iBKBAacBwtg6wcaJUtYQ61qUo6w3TsYXSCBoZ"
 ];
 
-var hdPublicKeys = [];
+var publicKeys = [];
+var privKeys = [];
+
+var changePublicKeys = [];
+var changePrivKeys = [];
 
 module.exports = function(app,db){
 	//Login
 	app.post('/login', (req, res) => {
 		//Mock user details received from post. 
-		//Save token locally and add to Header Bearer <token> in subsequent API calls
+		//Add token to Header Bearer <token> in subsequent API calls
 		const user = {
 			id: 1,
 			username: 'Tyrion',
@@ -37,7 +45,7 @@ module.exports = function(app,db){
 
 		jwt.sign({user}, secret, (err, token) => {
 			res.json({
-				token
+				Token: token
 			})
 		})
 	})
@@ -99,18 +107,23 @@ module.exports = function(app,db){
 
 				for (var i = hdPrivateKeys.length - 1; i >= 0; i--) {
 					
-					var hdPrivateKey = new HDPrivateKey(hdPrivateKeys[i]);
+					var privateKey = new HDPrivateKey(hdPrivateKeys[i]);
 					
-					hdPublicKeys[i] = hdPrivateKey.hdPublicKey.derive(bip_derive_path).publicKey;
+					publicKeys[i] = privateKey.hdPublicKey.derive(path).publicKey;
+					privateKeys[i] = privateKey;
+					privKeys[i] = privateKey.privateKey;
 				}
 
 				//Number of public keys required to generate a bitcoin address i.e 2/3
-				var requiredSignatures = 2;
+				var requiredSignatures = 3;
 
-				var address = new bitcore.Address(hdPublicKeys, requiredSignatures);
+				var address = new bitcore.Address(publicKeys, requiredSignatures, 'testnet');
 
 				res.json({
-					Bitcoin_address: address.toString()
+					Bitcoin_address: address.toString(),
+					Public_Keys: publicKeys,
+					Private_Keys: privateKeys,
+					Priv_Keys: privKeys
 				})
 			}
 		})
@@ -130,30 +143,50 @@ module.exports = function(app,db){
 
 				for (var i = hdPrivateKeys.length - 1; i >= 0; i--) {
 					
-					var hdPrivateKey = new HDPrivateKey(hdPrivateKeys[i]);
+					var hdPrivatekey = new HDPrivateKey(hdPrivateKeys[i]);
 					
-					hdPublicKeys[i] = hdPrivateKey.hdPublicKey.derive(bip_derive_path).publicKey;
+					publicKeys[i] = hdPrivatekey.hdPublicKey.derive(path).publicKey;
+					//privateKeys[i] = privatekey;
+					privKeys[i] = hdPrivatekey.derive(path).privateKey;
+
+					//Change Address Keys
+					changePublicKeys[i] = hdPrivatekey.hdPublicKey.derive(pathChange).publicKey;
+					changePrivKeys[i] = hdPrivatekey.derive(pathChange).privateKey;
 				}
 
 				//Number of public keys required to generate a bitcoin address i.e 2/3
-				var requiredSignatures = 2;
+				var requiredSignatures = 3;
 
 				//Generate change address
-				var address = new bitcore.Address(hdPublicKeys, requiredSignatures);
+				var address = new bitcore.Address(publicKeys, requiredSignatures, 'testnet');
+				var changeAddress = new bitcore.Address(changePublicKeys, requiredSignatures, 'testnet');
 
-				var utxo = {
+				/*var utxo = {
 				  "txId" : "153068cdd81b73ec9d8dcce27f2c77ddda12dee3db424bff5cafdbe9f01c1756",
 				  "outputIndex" : 0,
 				  "address" : address.toString(),
 				  "script" : new bitcore.Script(address).toHex(),
 				  "satoshis" : amount
-				};
+				};*/
+
+				var utxo = new UnspentOutput({
+				  "txId" : "c8547f4594e8cb43c39d85a147a76514e1131652be42ddf6b4ffb50aed600fae",
+				  "outputIndex" : 0,
+				  "address" : address.toString(),
+				  "script" : "a91471a7c8013ddde7ccec603cf450816e428813e0da87",
+				  "satoshis" : amount
+				});
 
 				var multiSigTx = new bitcore.Transaction()
-				    .from(utxo, hdPublicKeys, requiredSignatures)
-				    .to(to_address, amount)
-				    .change(address);
-				    //.sign(hdPrivateKeys);
+				    .from(utxo, publicKeys, requiredSignatures)
+				    .to(to_address, 90000)
+				    .change(changeAddress.toString())
+				    .fee(50000)
+				    .sign(privKeys);
+
+				    console.log(multiSigTx.toString());
+
+				    console.log(multiSigTx.serialize());
 
 				res.json({
 					Serialize: multiSigTx.toObject()
@@ -192,6 +225,19 @@ module.exports = function(app,db){
 			    res.json({
 					transaction: tx.toObject()
 				})
+			}
+		})
+	})
+
+	//validate address
+	app.post('/validate_address', verifyToken, (req, res) =>{
+		jwt.verify(req.token, secret, (err, authData) => {
+			if(err){
+				res.json({
+					error: error_msg
+				})
+			}else{
+				
 			}
 		})
 	})
